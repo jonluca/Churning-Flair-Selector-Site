07bc7e9e738434f755d13fb73c4631cb3e4838ba
JonLuca DeCaro <jdecaro@usc.edu>
2019-01-18 16:52:13 -0800

    more work on scheduler and poster

--- controller/post.js
+++ controller/post.js
@@ -4,6 +4,7 @@ const RedditApi = require("./reddit");
 const db = require("./db");
 const log = require('simple-node-logger').createSimpleLogger(path.join(__dirname, '../logs/activity.log'));
 
+const postPath = '/api/submit';
 const reddit = new RedditApi({
   app_id: config.mod_script_id,
   app_secret: config.mod_script_secret,
@@ -12,8 +13,39 @@ const reddit = new RedditApi({
 
 let PostController = {};
 
-PostController.createNewPost = (title, body, id, shouldSticky = true) => {
+PostController.createNewPost = (title, body, id, shouldSticky = true, cb) => {
+// Call authenticated POST endpoint
+  reddit.post(
+    postPath,
+    {
+      api_type: 'json',
+      kind: 'self',
+      nsfw: false,
+      text: body,
+      title: title,
+      sr: config.subreddit
+    },
+    function (error, response, body) {
+      console.log(body);
+      if (error) {
+        log.error(error);
+        return cb(false);
+      }
+      const resp = JSON.parse(body);
+      const data = resp.json.data;
 
+      db.insertCreatedPost(data.id, title, id, resp => {
+        if (resp) {
+          log.info(`Successfully saved post with id: ${data.id} in db`);
+          return;
+        }
+        log.error(`Error saving post with id: ${data.id} in db!`);
+      });
+
+      log.info(`Submitted post at ${(new Date()).toISOString()} with link ${data.url}`);
+      return cb(true);
+    }
+  );
 };
 
 PostController.removePost = (post, cb) => {
@@ -23,7 +55,8 @@ PostController.removePost = (post, cb) => {
 PostController.archiveOldById = id => {
 
 };
-PostController.refreshToken = _ => {
+PostController.refreshToken = (cb = _ => {
+}) => {
   // Authenticate with username/password
   reddit.passAuth(
     config.mod_username,
@@ -31,6 +64,7 @@ PostController.refreshToken = _ => {
     function (success) {
       if (success) {
         log.info("Successfully signed into mod account");
+        cb(true);
       } else {
         log.error("Error signing into mod account for flair assignment! Does config.js have the mod_username and mod_password fields?");
         process.exit(1);
@@ -39,7 +73,10 @@ PostController.refreshToken = _ => {
   );
 };
 
-PostController.refreshToken();
+PostController.refreshToken(_ => {
+  PostController.createNewPost("bot test", "# testing markdown\nand new lines", 0, true, _ => {
+  });
+});
 
 // Refresh the token every 30 minutes
 setInterval(_ => {
--- controller/scheduler.js
+++ controller/scheduler.js
@@ -9,11 +9,6 @@ Date.prototype.getMonthName = function (lang) {
   return Date.locale[lang].month_names[this.getMonth()];
 };
 
-Date.prototype.getMonthNameShort = function (lang) {
-  lang = lang && (lang in Date.locale) ? lang : 'en';
-  return Date.locale[lang].month_names_short[this.getMonth()];
-};
-
 Date.locale = {
   en: {
     month_names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
