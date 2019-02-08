54ebd635b91c4813c59f5316527cfb14295e5927
JonLuca DeCaro <jdecaro@usc.edu>
2019-02-07 21:58:42 -0800

    'removed test post creation'

--- controller/post.js
+++ controller/post.js
@@ -111,8 +111,6 @@ setInterval(_ => {
   log.info("Refreshed access token");
 }, 1000 * 60 * 30);
 PostController.refreshToken(_ => {
-  PostController.createNewPost("test", "d", 1, false, resp => {
-  });
 });
 
 module.exports = PostController;
